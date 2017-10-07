a486e076e100d103b04e8e2cf7a77e43d1c62e0d
ignatisD <ignatisnb@gmail.com>
2017-10-07 07:37:46 +0300

    Mongo and node

--- lampmenu.sh
+++ lampmenu.sh
@@ -6,14 +6,14 @@ while true; do
     echo "==============================="
     echo "Press [q] to quit this menu"
     echo ""
-    echo "1. Workspace"
-    echo "2. Php-fpm"
-    echo "3. Apache"
-    echo "4. Mysql"
-    echo "5. Beanstalkd"
-    echo "6. PhpMyAdmin"
-    echo "7. Redis"
-    echo "8. Memcached"
+    echo "1. Workspace root"
+    echo "2. Workspace non-root"
+    echo "3. Restart applications"
+    echo "4. Start node and mongo containers"
+    echo "5. Stop node and mongo containers"
+    echo "6. Node non-root user"
+    echo "7. Start Basic containers"
+    echo "8. Stop all containers"
     echo "9. Docker container list (ps)"
     echo "10. Start Basic containers"
     echo "11. Stop all containers"
@@ -31,45 +31,47 @@ while true; do
             docker-compose exec workspace bash
             ;;
         2)
-            docker-compose exec php-fpm bash
+            docker-compose exec --user laradock workspace bash
             ;;
         3)
-            docker-compose exec apache2 bash
+            docker-compose up applications
             ;;
         4)
-            docker-compose exec mysql bash
+            docker-compose up -d node mongo
             ;;
         5)
-            docker-compose exec beanstalkd bash
+            docker-compose stop node mongo
+            docker-compose rm -f node mongo
             ;;
         6)
-            docker-compose exec phpmyadmin bash
+            docker-compose exec --user node node bash
             ;;
         7)
-            docker-compose exec redis bash
+            docker-compose up -d apache2_nolara mysql phpmyadmin workspace php-fpm mailhog
+            read -p "Continue to menu..."
             ;;
         8)
-            docker-compose exec memcached bash
+            docker-compose stop
+            docker-compose rm -f
+            read -p "Continue to menu..."
             ;;
         9)
             docker-compose ps
             read -p "Continue to menu..."
             ;;
         10)
-            docker-compose up -d apache2_nolara mysql phpmyadmin workspace php-fpm mailhog
-            read -p "Continue to menu..."
-            ;;
-        11)
-            docker-compose stop
-            docker-compose rm -f
-            read -p "Continue to menu..."
-            ;;
-        12)
             docker-compose up -d mysql elasticsearch
             ;;
-        13)
+        11)
             docker-compose stop mysql elasticsearch
             docker-compose rm -f mysql elasticsearch
+            ;;
+        12)
+            docker-compose up -d redis memcached
+            ;;
+        13)
+            docker-compose stop redis memcached
+            docker-compose rm -f redis memcached
             ;;
         14)
             docker-compose up -d php-worker
@@ -81,7 +83,7 @@ while true; do
         16)
             docker-compose stop
             docker-compose rm -f
-            docker-compose build apache2 mysql redis memcached beanstalkd elasticsearch workspace php-fpm mailhog phpmyadmin
+            docker-compose build apache2 mysql redis memcached beanstalkd elasticsearch workspace php-fpm mailhog phpmyadmin node mongo
             read -p "Continue to menu..."
             ;;
         q)
