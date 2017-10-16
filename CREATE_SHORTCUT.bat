dd20986eaa138c69742c20a60d2a6fa17f3b6d43
Ignatis <ignatisnb@gmail.com>
2017-10-16 09:55:32 +0300

    Create lamp stack bat

--- CREATE_SHORTCUT.bat
+++ CREATE_SHORTCUT.bat
@@ -3,11 +3,11 @@ set currentpath=%cd%
 cd /d %currentpath%
 
 echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
-echo sLinkFile = "%userprofile%\Desktop\Lamp stack MENU.lnk" >> CreateShortcut.vbs
+echo sLinkFile = "%userprofile%\Desktop\LAMP-MEAN MENU.lnk" >> CreateShortcut.vbs
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
 echo oLink.TargetPath = "%currentpath%\lampmenu.sh" >> CreateShortcut.vbs
 echo oLink.WorkingDirectory = "%currentpath%" >> CreateShortcut.vbs
-echo oLink.Description = "Lamp Stack Menu" >> CreateShortcut.vbs
+echo oLink.Description = "LAMP and MEAN Stack Menu" >> CreateShortcut.vbs
 echo oLink.IconLocation = "%currentpath%\icons\lamp-stack.ico" >> CreateShortcut.vbs
 echo oLink.Save >> CreateShortcut.vbs
 cscript CreateShortcut.vbs
