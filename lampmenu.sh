f4b48fb2ad3438dfc6c71c609bd4147767670770
Ignatis <ignatisnb@gmail.com>
2017-10-16 09:51:55 +0300

    Lamp && Mean stack with docker

--- lampmenu.sh
+++ lampmenu.sh
@@ -6,21 +6,21 @@ while true; do
     echo "==============================="
     echo "Press [q] to quit this menu"
     echo ""
-    echo "1. Workspace root"
-    echo "2. Workspace non-root"
-    echo "3. Restart applications"
-    echo "4. Start node and mongo containers"
-    echo "5. Stop node and mongo containers"
-    echo "6. Node non-root user"
-    echo "7. Start Basic containers"
-    echo "8. Stop all containers"
+    echo "1. Start LAMP containers"
+    echo "2. Workspace root bash"
+    echo "3. Workspace non-root bash"
+    echo "4. Php-fpm root bash"
+    echo "5. Start MEAN containers"
+    echo "6. Node root user"
+    echo "7. Node non-root user"
+    echo "8. Stop node and mongo containers"
     echo "9. Docker container list (ps)"
-    echo "10. Start Basic containers"
-    echo "11. Stop all containers"
-    echo "12. Start Redis and Memcached containers"
-    echo "13. Stop Redis and Memcached containers"
-    echo "14. Start elasticsearch extra container"
-    echo "15. Stop elasticsearch extra container"
+    echo "10. Stop all containers"
+    echo "11. Start Mysql container"
+    echo "12. Start PhpMyAdmin container"
+    echo "13. Start Mailhog container"
+    echo "14. Start php-worker container"
+    echo "15. Start Redis, Memcached and Elasticsearch containers"
     echo "16. Rebuild all containers"
     echo ""
     echo -n "Select a number or type a command: "
@@ -28,30 +28,29 @@ while true; do
 
     case "$input" in
          1)
-             docker-compose exec workspace bash
-             ;;
-         2)
-             docker-compose exec --user laradock workspace bash
-             ;;
-         3)
-                docker-compose up applications
-             ;;
-         4)
-             docker-compose up -d node mongo
-             ;;
-         5)
-             docker-compose stop node mongo
-             docker-compose rm -f node mongo
-             ;;
-         6)
-             docker-compose exec --user node node bash
-             ;;
-         7)
-             docker-compose up -d apache2_nolara mysql phpmyadmin workspace php-fpm mailhog
+             docker-compose up -d apache2 mysql workspace php-fpm mailhog
              read -p "Continue to menu..."
              ;;
+         2)
+             docker-compose exec workspace bash
+             ;;
+         3)
+             docker-compose exec --user laradock workspace bash
+             ;;
+         4)
+             docker-compose exec php-fpm bash
+             ;;
+         5)
+             docker-compose up -d node mongo mailhog
+             ;;
+         6)
+             docker-compose exec node bash
+             ;;
+         7)
+             docker-compose exec --user node node bash
+             ;;
          8)
-             docker-compose stop
+             docker-compose stop node mongo
              docker-compose rm -f
              read -p "Continue to menu..."
              ;;
@@ -60,38 +59,38 @@ while true; do
              read -p "Continue to menu..."
              ;;
          10)
-             docker-compose up -d mysql elasticsearch
+             docker-compose stop
+             docker-compose rm -f
+             read -p "Continue to menu..."
              ;;
          11)
-             docker-compose stop mysql elasticsearch
-             docker-compose rm -f mysql elasticsearch
+             docker-compose up -d mysql
              ;;
          12)
-             docker-compose up -d redis memcached
+             docker-compose up -d phpmyadmin
              ;;
          13)
-             docker-compose stop redis memcached
-             docker-compose rm -f redis memcached
+             docker-compose up -d mailhog
              ;;
          14)
              docker-compose up -d php-worker
              ;;
          15)
-             docker-compose stop php-worker
-             docker-compose rm -f php-worker
+             docker-compose up -d redis memcached elasticsearch
              ;;
          16)
              docker-compose stop
              docker-compose rm -f
-             docker-compose build apache2 mysql redis memcached beanstalkd elasticsearch workspace php-fpm mailhog phpmyadmin node mongo
+             docker-compose build apache2 mysql workspace php-fpm mailhog node mongo
+             docker-compose build redis memcached beanstalkd elasticsearch phpmyadmin
              read -p "Continue to menu..."
              ;;
          q)
              exit
              ;;
          *)
- 			$input
- 			echo ""
+ 			 $input
+ 			 echo ""
              read -p "Continue to menu..."
              ;;
      esac
