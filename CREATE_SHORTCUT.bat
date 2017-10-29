33c75e732c9319bee5202fb84f541fdb9c6d672e
ignatisD <ignatisnb@gmail.com>
2017-10-29 21:39:56 +0200

    Windows create shortcut

--- CREATE_SHORTCUT.bat
+++ CREATE_SHORTCUT.bat
@@ -3,9 +3,10 @@ set currentpath=%cd%
 cd /d %currentpath%
 
 echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
-echo sLinkFile = "%userprofile%\Desktop\LAMP-MEAN MENU.lnk" >> CreateShortcut.vbs
+echo sLinkFile = "%userprofile%\Desktop\Lamp Stack menu.lnk" >> CreateShortcut.vbs
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
-echo oLink.TargetPath = "%currentpath%\lampmenu.sh" >> CreateShortcut.vbs
+echo oLink.TargetPath = "%programfiles%\Git\bin\bash.exe" >> CreateShortcut.vbs
+echo oLink.Arguments  = "-l %currentpath%\lampmenu.sh" >> CreateShortcut.vbs
 echo oLink.WorkingDirectory = "%currentpath%" >> CreateShortcut.vbs
 echo oLink.Description = "LAMP and MEAN Stack Menu" >> CreateShortcut.vbs
 echo oLink.IconLocation = "%currentpath%\icons\lamp-stack.ico" >> CreateShortcut.vbs
